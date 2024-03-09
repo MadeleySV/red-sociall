71d0c78b154eced45111ffee3e5e7ff42b410503
shekinahNavarro <navarroshekinah31@gmail.com>
2024-03-08 19:27:54 -0600

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,23 @@ import './firebase/google.js';
 import './firebase/signinModal.js';
 import './firebase/firebase.js';
 import './firebase/registro.js';
+
+
+// Agregar un evento al formulario para redirigir después de iniciar sesión
+document.getElementById('register-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evitar que el formulario se envíe
+    // Aquí se verificaría el inicio de sesión y si es exitoso, se redirigiría al usuario
+    window.location.href = 'index2.html';
+  });
+
+document.getElementById('signinForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evitar que el formulario se envíe
+    // Aquí se verificaría el inicio de sesión y si es exitoso, se redirigiría al usuario
+    window.location.href = 'index2.html';
+  });
+
+  document.getElementById('google-login').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evitar que el formulario se envíe
+    // Aquí se verificaría el inicio de sesión y si es exitoso, se redirigiría al usuario
+    window.location.href = 'index2.html';
+  });
